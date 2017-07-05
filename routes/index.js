24a03fea09a7fd990b9ab5d96fed9313ff144dfc
Rui(Ray) Liu <rogerliuray@gmail.com>
2017-07-05 08:24:31 -0400

    db integration

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 var express = require('express')
 var router = express.Router()
+var mongojs = require('mongojs')
+var db = mongojs('local', ['users'])
 const DarkSky = require('dark-sky')
-//https://www.npmjs.com/package/node-geocoder
+// https://www.npmjs.com/package/node-geocoder
 var NodeGeocoder = require('node-geocoder')
-
+var sess
 // Use synchronous JavaScript call to fetch API id stored in forecast.txt.
 // Use API id fetched from file system.
 const forecast = new DarkSky('a663e12e77b9cfe68f9151767de5a597')
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('signinup', { title: 'Express' })
+  res.render('index', { title: 'Express' })
 })
 router.post('/today', function (req, res) {
   let now = new Date()
@@ -32,5 +34,48 @@ router.post('/today', function (req, res) {
     .catch(err => {                 // handle your error response.
       console.log(err)
     })
+})
+
+router.post('/login', function (req, res) {
+    // 200 OK
+  res.status(200)
+  console.log('POST - localhost:3000/login')
+        // Parse body
+  console.log(req.body)
+  var id = JSON.stringify(req.body.uname)
+    // //only search by id.
+  var data = '{' + '"id": ' + id + '}'
+    // //Convert String to JSON
+  data = JSON.parse(data)
+    // Query Body
+  db.user.find(data, function (err, records) {
+    if (err) {
+      console.log('Database Error' + err)
+      res.send('Database Error' + err)
+    } else {
+            // No error, but no result returned
+      if (records[0] === undefined) {
+        console.log('User does not exist!')
+        res.send('User does not exist!')
+      } else {
+                // Result returned, user exist.
+        console.log('User found!')
+        console.log(records[0])
+        if (records[0].password === req.body.password) {
+                    // Password matched.
+          console.log('Password Matched, redirecting....')
+          sess = req.session
+          sess.userType = records[0].usertype
+          sess.fname = records[0].firstname
+          sess.email = records[0].email
+          sess.user_id = records[0].id
+          res.render('user', { title: 'Express' })
+        } else {
+          console.log('Wrong Password!')
+          res.send('Wrong Password!')
+        }
+      }
+    }
+  })
 })
 module.exports = router
