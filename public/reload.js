ddcddd17057efb35d62009b0462c20829ee09799
Rui(Ray) Liu <rogerliuray@gmail.com>
2017-07-13 09:22:16 -0400

    initMap

--- public/reload.js
+++ public/reload.js
@@ -230,16 +230,7 @@ function reload(lat, lng, unit) {
         $('span.inner').replaceWith('<span class="inner"><h3 style="text-align:right"><span>' + roundTemp(data.currently.temperature) + ' Cº</span></h3><h5 style="text-align:right">' + 'H: ' + roundTemp(data.daily.data[0].temperatureMax) + ' Cº ' + 'L: ' + roundTemp(data.daily.data[0].temperatureMin) + ' Cº</h5></span>');
         $('span.sum').replaceWith('<span class="sum"><h5>' + data.currently.summary + '</h5></span>');
         $.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + lng + '&key=AIzaSyAOUiSYFZUxtHi6zk3cqIYl7TOyPusI6fE', function (data2) {
-            console.log(data2.results[4].formatted_address);
-            console.log(data2);
-            //            var short = data2.results[0];
-            //            for (var i = 1; i< data2.results.length; i++){
-            //                if (data2.results[i].formatted_address.length>)
-            //                console.log(data2.results[i]);
-            //                
-            //            }
             $('span.location2').replaceWith('<span class="location2"><h3>' + data2.results[4].formatted_address + '</h3></span>')
-                // var results = data.results;
         })
     })
 }
